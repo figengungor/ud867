d5a97d63a3245b1d33860a285568bead3f80fe46
figengungor <figengungor1@gmail.com>
2018-05-07 11:52:21 +0300

    Create a Java library provides jokes

---
+++ FinalProject/jokeTeller/src/main/java/com/figengungor/joketeller/Joke.java
@@ -0,0 +1,9 @@
+package com.figengungor.joketeller;
+
+public class Joke {
+
+    public String getJoke(){
+        return "Want to hear a joke about construction? I'm still working on it.";
+    }
+
+}
