3e895a106d00c91a2ab5bf5d16f81bd5a3619b3a
figengungor <figengungor1@gmail.com>
2018-05-07 12:10:13 +0300

    Create an Android library to display the passed joke in Activity

--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.figengungor.jokedisplayer.JokeActivity;
 import com.figengungor.joketeller.Joke;
 
 
@@ -43,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void tellJoke(View view) {
         Joke joke = new Joke();
-        Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, JokeActivity.class)
+                .putExtra(JokeActivity.EXTRA_JOKE, joke.getJoke()));
     }
 
 
---
+++ FinalProject/jokeDisplayer/src/main/java/com/figengungor/jokedisplayer/JokeActivity.java
@@ -0,0 +1,26 @@
+package com.figengungor.jokedisplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public static final String EXTRA_JOKE = "joke";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView jokeTv = findViewById(R.id.jokeTv);
+        String joke = getIntent().getExtras().getString(EXTRA_JOKE);
+        jokeTv.setText(joke);
+
+    }
+
+}
