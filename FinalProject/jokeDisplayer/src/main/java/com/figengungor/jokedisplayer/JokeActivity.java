6d3daf2bd1321ba33b58cb73fbedf79d78d88538
figengungor <figengungor1@gmail.com>
2018-05-09 13:06:40 +0300

    Add up navigation to JokeActivity

--- FinalProject/jokeDisplayer/src/main/java/com/figengungor/jokedisplayer/JokeActivity.java
+++ FinalProject/jokeDisplayer/src/main/java/com/figengungor/jokedisplayer/JokeActivity.java
@@ -3,6 +3,7 @@ package com.figengungor.jokedisplayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
@@ -13,13 +14,21 @@ public class JokeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView jokeTv = findViewById(R.id.jokeTv);
         String joke = getIntent().getExtras().getString(EXTRA_JOKE);
         jokeTv.setText(joke);
-
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
